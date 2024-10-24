3bd6a33f6004e2ad648e46bc9311efe287f82bda
LAURA CRISTINA MELEK NUNES <laura_c_nunes@estudante.sesisenai.org.br>
2024-10-24 14:53:10 -0300

    legal

--- S.A/src/App.jsx
+++ S.A/src/App.jsx
@@ -1,35 +1,20 @@
-<<<<<<< HEAD
+
 
 import { useState } from 'react'
 import Boas_Vindas from "./pages/Boas_Vindas"
 import Cadastro from "./pages/Cadastro"
 import Estrategia1 from "./estrategias/Estrategia1"
-=======
-import { useState } from 'react'
->>>>>>> e07bef39660f31976442305e6bd66744d3e81966
 import './App.css'
-import Boas_Vindas from './pages/Boas_Vindas'
 import NavBar_Principal from './components/NavBar_Principal'
 import Tela_Principal from './pages/Tela_Principal'
 
 function App() {
-<<<<<<< HEAD
+
   return (
     <>
-    
-<Boas_Vindas/>
-<Cadastro />
-<Estrategia1 />
+
 
     </>
-=======
-
-
-  return (
-   
-  <Tela_Principal/>
-
->>>>>>> e07bef39660f31976442305e6bd66744d3e81966
   )
 }
 
